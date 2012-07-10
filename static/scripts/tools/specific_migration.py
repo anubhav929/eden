680e0c087ee74c00d78360ea34d57ae65c0b619a
root <root@ubuntu.ubuntu-domain>
2012-07-10 20:20:14 +0530

    correct typos , renamed fields , improved test script for the migration tool

--- static/scripts/tools/specific_migration.py
+++ static/scripts/tools/specific_migration.py
@@ -3,8 +3,11 @@ import sys
 import copy
 import subprocess
 
-WEB2PY_PATH = "/home/web2py"
-OLD_APP = "eden3"
+own_path = os.path.realpath(__file__)
+own_path = own_path.split(os.path.sep)
+index_application = own_path.index("applications")
+WEB2PY_PATH = (os.path.sep).join(own_path[0:index_application])
+APP = "eden"
 changed_table = "org_organisation"
 new_field = "type_id"
 new_table = "org_organisation_type"
@@ -19,13 +22,13 @@ custom_import_install(WEB2PY_PATH)
 from gluon.shell import env
 from gluon import DAL, Field
 
-old_env = env(OLD_APP, c=None, import_models=True)
+old_env = env(APP, c=None, import_models=True)
 old_str ='''
 try:
     s3db.load_all_models()
 except NameError:
     print "s3db not defined"
-old_db = db
+db = db
 '''
 d = globals().copy()
 d.update(**old_env)
@@ -34,28 +37,25 @@ exec old_str in d, locals()
 d.clear()
 
 database_string = "sqlite://storage.db"
-old_database_folder = "%s/applications/%s/databases" % (WEB2PY_PATH, OLD_APP)
+old_database_folder = "%s/applications/%s/databases" % (WEB2PY_PATH, APP)
 temp_db = DAL( database_string, folder = old_database_folder, migrate_enabled=True ,migrate = True)
 
 #MIGRATION SCRIPT
 
-list_of_new_fields = []
+list_of_fields = []
 list_of_fields.append(Field(new_field,"integer"))
 
 list_of_new_table_fields = []
-list_of_new_table_fields .append(Field(new_table_field,"integer"))
+list_of_new_table_fields.append(Field(new_table_field,"integer"))
 
-temp_db.define_table(changed_table ,old_db[changed_table],*list_of_fields)
+temp_db.define_table(changed_table ,db[changed_table],*list_of_fields)
 temp_db.define_table(new_table , *list_of_new_table_fields)
 
 temp_db.commit()
 
 #Adding a new field of int type in a class
 
-old_rows = []
-old_rows.extend(temp_db().select(temp_db[changed_table][old_field]))
-
-for old_row in old_rows:
+for old_row in temp_db().select(temp_db[changed_table][old_field]):
     if (len(temp_db(temp_db[new_table][new_table_field] == old_row[old_field]).select()) == 0):
         temp_db[new_table].insert(name = old_row[old_field])
     new_id = int(temp_db(temp_db[new_table][new_table_field] == old_row[old_field]).select(temp_db[new_table]["id"])[0]["id"])
--- tests/dbmigration/TestMigration.py
+++ tests/dbmigration/TestMigration.py
@@ -2,9 +2,18 @@ import os
 import sys
 import subprocess
 
-WEB2PY_PATH = "/home/web2py"
-OLD_APP = "eden3"
+own_path = os.path.realpath(__file__)
+own_path = own_path.split(os.path.sep)
+index_application = own_path.index("applications")
+CURRENT_EDEN_APP  = own_path[index_application + 1]
+WEB2PY_PATH = (os.path.sep).join(own_path[0:index_application])
+APP = "eden"
 changed_table = "org_organisation"
+new_field = "type_id"
+old_field = "type"
+new_table_field = "name"
+new_table = "org_organisation_type"
+
 
 os.chdir(WEB2PY_PATH)
 sys.path.append(WEB2PY_PATH)
@@ -14,13 +23,13 @@ custom_import_install(WEB2PY_PATH)
 from gluon.shell import env
 from gluon import DAL, Field
 
-old_env = env(OLD_APP, c=None, import_models=True)
+old_env = env(APP, c=None, import_models=True)
 old_str ='''
 try:
     s3db.load_all_models()
 except NameError:
     print "s3db not defined"
-old_db = db
+db = db
 '''
 d = globals().copy()
 d.update(**old_env)
@@ -29,12 +38,24 @@ exec old_str in d, locals()
 d.clear()
 
 for a in range(10):
-    old_db[changed_table].insert(name = "test_%s" %(str(a)), type = a%5 , uuid = "%s%s" %(old_db[changed_table]["uuid"].default,str(a)))
+    db[changed_table].insert(name = "test_%s" %(str(a)), type = a%5 , uuid = "%s%s" %(db[changed_table]["uuid"].default,str(a)))
 
-old_db.commit()
-subprocess.call("python %s/applications/eden/static/scripts/tools/miration.py" % \
-         (WEB2PY_PATH) , shell =True )
+db.commit()
+subprocess.call("python %s/applications/%s/static/scripts/tools/specific_migration.py" % \
+         (WEB2PY_PATH, CURRENT_EDEN_APP) , shell =True )
 
 """
 Retreiving Data
 """
+
+database_string = "sqlite://storage.db"
+old_database_folder = "%s/applications/%s/databases" % (WEB2PY_PATH, APP)
+db = DAL( database_string, folder = old_database_folder, auto_import = True, migrate_enabled=True ,migrate = True)
+
+print "CHANGED TABLE :"
+for row in db(db[changed_table]).select():
+	print "name = ",row["name"],"old_field = ",row[old_field],"new_field = ",row[new_field]
+
+print "NEW TABLE :"
+for row in db(db[new_table]).select():
+	print "name = ",row[new_table_field],"id = ",row["id"]
