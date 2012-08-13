f67e8ec3816154fdd7d8e692fbf0079383230aeb
root <root@ubuntu.ubuntu-domain>
2012-08-13 17:07:49 +0530

    improved list functionality

--- static/scripts/tools/list_fields_to_references.py
+++ static/scripts/tools/list_fields_to_references.py
@@ -31,25 +31,20 @@ except NameError:
     return [db,dbengine]
 
 def map_type(old_type):
-    if ((old_type == "list:integer") or (old_type.startswith("list:reference"))):
+    if (old_type == "list:integer"):
         return "integer"
+    elif old_type.startswith("list:reference"):
+        return old_type.strip("list:")
     elif old_type == "list:string":
         return "string"
 
-def creating_new_table(db , new_table_name , new_list_field , list_field_name , old_table_id_field , old_table,list_referncing_table = None):
-    database_string = "sqlite://storage.db"
-    old_database_folder = "%s/applications/%s/databases" % (WEB2PY_PATH, APP)
-    temp_db = DAL( database_string, folder = old_database_folder, migrate_enabled=True ,migrate = True)
+def creating_new_table(db , new_table_name , new_list_field , list_field_name , old_table_id_field , old_table):
     new_field_type = map_type(db[old_table][list_field_name].type)
     new_field = Field(new_list_field,new_field_type)
-    new_id_field = Field("%s_%s" %(old_table,old_table_id_field),"integer")
-    temp_db.define_table(new_table_name ,new_id_field , new_field )
-    temp_db.define_table(old_table ,db[old_table])
-    if list_referncing_table:
-        temp_db.define_table(list_referncing_table ,db[list_referncing_table])  
-    temp_db.commit()
-    return temp_db
-
+    new_id_field = Field("%s_%s" %(old_table,old_table_id_field),"reference %s" % ( old_table))
+    db.define_table(new_table_name ,new_id_field , new_field )
+    db.commit()
+    
 def fill_the_new_table(db , new_table_name , new_list_field , list_field_name , old_table_id_field , old_table ):
     update_dict = {}
     for row in db().select(db[old_table][old_table_id_field],db[old_table][list_field_name]):
@@ -57,7 +52,7 @@ def fill_the_new_table(db , new_table_name , new_list_field , list_field_name , 
             update_dict[new_list_field] = element
             update_dict["%s_%s" %(old_table,old_table_id_field)] = row[old_table_id_field]
             db[new_table_name].insert(**update_dict)
-    temp_db.commit()
+    db.commit()
     
         
 dbengine = ""
@@ -69,7 +64,6 @@ new_table_name = "sector_id_reference"
 new_list_field = list_field_name = "sector_id"
 old_table_id_field = "id"
 old_table = "org_organisation"
-list_referncing_table = "org_sector"
 
-db = creating_new_table(db , new_table_name , new_list_field , list_field_name , old_table_id_field , old_table,list_referncing_table)
+creating_new_table(db , new_table_name , new_list_field , list_field_name , old_table_id_field , old_table)
 fill_the_new_table(db , new_table_name , new_list_field , list_field_name , old_table_id_field , old_table )
